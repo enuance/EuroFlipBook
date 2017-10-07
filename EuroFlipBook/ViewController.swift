d55080079b5c8f2a0b8eb12b8b34f51b60c2618a
Stephen Martinez <enharmonicnuance@gmail.com>
2017-10-06 17:02:24 -0700

    Initial Commit

---
+++ EuroFlipBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EuroFlipBook
+//
+//  Created by Stephen Martinez on 10/6/17.
+//  Copyright © 2017 Stephen Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
